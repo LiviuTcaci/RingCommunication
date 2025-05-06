49cdd3eb5511783b4a89fa56fdd439567d7b2383
LiviuTcaci <tcaci.liviu@gmail.com>
2025-05-06 20:26:10 +0300

    Implement server logic for receiving and incrementing value

--- src/Node.java
+++ src/Node.java
@@ -15,10 +15,57 @@ public class Node {
     }
 
     public void start() {
-        // TODO: Implement server and client logic
-        // Server: listen on localPort, receive value, increment, send to next node
-        // Client: connect to nextIP:nextPort, send value
-        System.out.println("Node started on port " + localPort + ", next hop: " + nextIP + ":" + nextPort);
+        try {
+            // Server: listen on localPort
+            ServerSocket serverSocket = new ServerSocket(localPort);
+            System.out.println("Node started on port " + localPort + ", next hop: " + nextIP + ":" + nextPort);
+
+            // If this is the initiator, start by sending value 1
+            if (isInitiator) {
+                sendValue(1);
+            }
+
+            while (true) {
+                // Accept a connection
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Connection accepted from " + clientSocket.getInetAddress());
+
+                // Read the value from the client
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                String valueStr = in.readLine();
+                int value = Integer.parseInt(valueStr);
+                System.out.println("Received value: " + value);
+
+                // Increment the value
+                value++;
+                System.out.println("Incremented value: " + value);
+
+                // If value < 100, send to next node
+                if (value < 100) {
+                    sendValue(value);
+                } else {
+                    System.out.println("Value reached 100, stopping communication.");
+                }
+
+                // Close the connection
+                clientSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendValue(int value) {
+        try {
+            // Connect to the next node
+            Socket socket = new Socket(nextIP, nextPort);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(value);
+            System.out.println("Sent value " + value + " to " + nextIP + ":" + nextPort);
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
